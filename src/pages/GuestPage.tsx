94213aac63a53954b0f9f24c449afc4184045f5c
beboom81 <beboom8119@gmail.com>
2026-06-08 18:43:50 +0700

    Fix name and date

--- src/pages/GuestPage.tsx
+++ src/pages/GuestPage.tsx
@@ -15,7 +15,7 @@ const LOVE_PATH =
   'm8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15';
 
 const COUPLE = 'Duy Khanh & Ý Duyên';
-const PLACE_DATE = 'Hồ Chí Minh, 13 October 2026';
+const PLACE_DATE = 'Hồ Chí Minh, 01 August 2026';
 
 const gallery1 = [1, 2, 3];
 const gallery2 = [4, 5, 6];
@@ -322,13 +322,13 @@ export default function GuestPage() {
                       Trưởng nữ
                     </p>
                     <p className="mb-0" style={{ fontSize: '0.95rem' }}>
-                      Cha Phạm Văn A
+                      Cha Phạm Vũ Bính
                     </p>
                     <p className="mb-0" style={{ fontSize: '0.95rem' }}>
                       &
                     </p>
                     <p className="mb-0" style={{ fontSize: '0.95rem' }}>
-                      Mẹ Nguyễn Thị Nguyệt
+                      Mẹ Nguyễn Thị Thu Nguyệt
                     </p>
                   </div>
                   <Love top="90%" left="5%" />
